2412f4d1568471095bd2fe6efe65977e43e83538
Trần Xuân Bách <tranxuanbach1@gmail.com>
2021-10-13 17:36:34 +0700

    Complete some related class API entries 1) Create Class (only Teacher) 2) Add student to class (by VNU_ID) 3) Get current class of member (both teachers and students)

--- server/routers/class.js
+++ server/routers/class.js
@@ -1,9 +1,10 @@
 const classRouter = require("express").Router();
 const Configs = require("./../configs/Constants");
 const {validateToken, validateLoginArgument, login, } = require("../middleware/auth-middleware/auth");
-const {createClass, validateClassTeacher, addMembersToClass} = require("../middleware/class-middleware/class");
+const {createClass, validateClassTeacher, addMembersToClass, getCurClass} = require("../middleware/class-middleware/class");
 // classRouter.get('/auth/test', validateToken);
 // authRouter.post('/auth/reg', register);
 classRouter.post(Configs.API_PATH.CREATE_CLASS, validateToken, createClass);
-classRouter.post(Configs.API_PATH.ADD_MEMBER_CLASS, validateToken, validateClassTeacher, addMembersToClass)
+classRouter.post(Configs.API_PATH.ADD_MEMBER_CLASS, validateToken, validateClassTeacher, addMembersToClass);
+classRouter.get(Configs.API_PATH.MY_CLASS, validateToken, getCurClass)
 module.exports = classRouter

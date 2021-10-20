4352ed93428c7a05077750d54279719e01cd6659
Trần Xuân Bách <tranxuanbach1@gmail.com>
2021-10-20 13:29:54 +0700

    Fix useRecoilValue bugs V.Anh

--- client/src/_components/feed/Feed.jsx
+++ client/src/_components/feed/Feed.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import { useRecoilValue } from 'recoil';
+import { useRecoilState, useRecoilValue } from 'recoil';
 
 import { studentsAtom } from '_state';
 
@@ -9,8 +9,7 @@ export { Feed };
 
 function Feed() {
 
-    const [student,setStudent] = useRecoilValue(studentsAtom);
-
+    const student = useRecoilState(studentsAtom);
     return (
         <div className="p-4">
             <StudentInfoList data={student}/>

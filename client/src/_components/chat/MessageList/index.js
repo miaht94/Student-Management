ff54239a518594cc87fa30a247bfdc3a4b6e17b2
Viet-Anh Nguyen <61254534+vakoyomi@users.noreply.github.com>
2021-10-29 15:49:55 +0700

    [Working] added send button

--- client/src/_components/chat/MessageList/index.js
+++ client/src/_components/chat/MessageList/index.js
@@ -5,6 +5,9 @@ import ToolbarButton from '../ToolbarButton';
 import Message from '../Message';
 import moment from 'moment';
 
+import { Button, Tooltip } from 'antd';
+import { SendOutlined } from '@ant-design/icons';
+
 import './MessageList.css';
 
 const MY_USER_ID = 'apple';
@@ -175,12 +178,9 @@ export default function MessageList(props) {
         <div className="message-list-container">{renderMessages()}</div>
 
         <Compose rightItems={[
-          <ToolbarButton key="photo" icon="ion-ios-camera" />,
-          <ToolbarButton key="image" icon="ion-ios-image" />,
-          <ToolbarButton key="audio" icon="ion-ios-mic" />,
-          <ToolbarButton key="money" icon="ion-ios-card" />,
-          <ToolbarButton key="games" icon="ion-logo-game-controller-b" />,
-          <ToolbarButton key="emoji" icon="ion-ios-happy" />
+          <Tooltip title="Gửi" className="toolbar-button">
+          <Button type="primary" shape="circle" icon={<SendOutlined />} size="large" />
+          </Tooltip>
         ]}/>
       </div>
     );

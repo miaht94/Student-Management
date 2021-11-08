22860e55bf18a75b1bcddd438a6ae03fed70daee
Trần Xuân Bách <tranxuanbach1@gmail.com>
2021-11-08 20:06:27 +0700

    Add get class scores

--- server/routers/score.js
+++ server/routers/score.js
@@ -1,11 +1,13 @@
 const scoreRouter = require("express").Router();
 const Configs = require("./../configs/Constants");
 const {validateToken, validateLoginArgument, login, } = require("../middleware/auth-middleware/auth");
-const {checkTargetAddScoreExist, checkTeacherOfVNUId, fAddScoreToScoresTable, fGetScoresByVNUId, fHandleUploadScore } = require("../middleware/score-middleware/score");
+const {fGetScoresClassByClassId, checkTargetAddScoreExist, checkTeacherOfVNUId, fAddScoreToScoresTable, fGetScoresByVNUId, fHandleUploadScore } = require("../middleware/score-middleware/score");
 const { handleUploadFile } = require("../middleware/upload-middleware/upload");
+const {fFindClassByClassId, fCreateClass, validateClassTeacher, fAddMembersToClass, fGetCurClasses, handleUploadMembers, findClassByClassId, validateClassMember, fGetMemberBasicInfors, fDeleteMemberInClass} = require("../middleware/class-middleware/class");
 // classRouter.get('/auth/test', validateToken);
 // authRouter.post('/auth/reg', register);
 scoreRouter.get(Configs.API_PATH.GET_SCORES_BY_ID, validateToken, checkTeacherOfVNUId, fGetScoresByVNUId);
 scoreRouter.post(Configs.API_PATH.ADD_SCORE_BY_VNU_ID, checkTargetAddScoreExist, fAddScoreToScoresTable);
 scoreRouter.post(Configs.API_PATH.UPLOAD_SV_MH_SCORE, handleUploadFile, fHandleUploadScore);
+scoreRouter.get(Configs.API_PATH.GET_SCORES_CLASS, validateToken, findClassByClassId,  validateClassTeacher, fGetScoresClassByClassId)
 module.exports = scoreRouter;

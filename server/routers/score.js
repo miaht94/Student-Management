e61a0b8e090b8dbaae31ac18a8c91e0ff01aab29
Trần Xuân Bách <tranxuanbach1@gmail.com>
2021-12-07 03:55:54 +0700

    Modify download score to post

--- server/routers/score.js
+++ server/routers/score.js
@@ -6,7 +6,7 @@ const { handleUploadFile } = require("../middleware/upload-middleware/upload");
 const {fFindClassByClassId, fCreateClass, validateClassTeacher, fAddMembersToClass, fGetCurClasses, handleUploadMembers, findClassByClassId, validateClassMember, fGetMemberBasicInfors, fDeleteMemberInClass} = require("../middleware/class-middleware/class");
 // classRouter.get('/auth/test', validateToken);
 // authRouter.post('/auth/reg', register);  
-scoreRouter.get(Configs.API_PATH.DOWNLOAD_SCORES_CLASS, validateToken, findClassByClassId, validateClassTeacher, fDownloadScoresClassByClassId);
+scoreRouter.post(Configs.API_PATH.DOWNLOAD_SCORES_CLASS, validateToken, findClassByClassId, validateClassTeacher, fDownloadScoresClassByClassId);
 scoreRouter.post(Configs.API_PATH.ADD_SCORE_BY_VNU_ID, checkTargetAddScoreExist, fAddScoreToScoresTable);
 scoreRouter.post(Configs.API_PATH.UPLOAD_SV_MH_SCORE, handleUploadFile, fHandleUploadScore);
 scoreRouter.get(Configs.API_PATH.GET_SCORES_BY_ID, validateToken, checkTeacherOfVNUId, fGetScoresByVNUId);

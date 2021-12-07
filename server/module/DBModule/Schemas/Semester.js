8d5731ca5a4bbcb838c62e9c3c9c67a908f9c4a3
Trần Xuân Bách <tranxuanbach1@gmail.com>
2021-12-08 01:51:10 +0700

    Them ngay bat dau ket thuc ky hoc

--- server/module/DBModule/Schemas/Semester.js
+++ server/module/DBModule/Schemas/Semester.js
@@ -5,5 +5,7 @@ const ObjectId = Schema.Types.ObjectId;
 const SemesterSchema = new Schema({
     semester_id: {type: String, required : true, unique: true, index: true},
     semester_name: {type: String, required: true},
+    start_date: {type: Number, required: false, default: null},
+    end_date: {type: Number, required: false, default: null}
 })
 module.exports = {SemesterSchema};

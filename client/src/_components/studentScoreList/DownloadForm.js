56b2cdfcbae8212ab0430c1bf764f188a456d458
Viet-Anh Nguyen <61254534+vakoyomi@users.noreply.github.com>
2021-12-08 06:17:47 +0700

    [Fixed] DownloadForm

--- client/src/_components/studentScoreList/DownloadForm.js
+++ client/src/_components/studentScoreList/DownloadForm.js
@@ -1,13 +1,53 @@
 import React ,{useEffect, useState} from 'react';
 import 'antd/dist/antd.css';
-import { Form, Input, Button, Divider} from 'antd';
-import { useClassWrapper } from '_helpers';
+import { Form, Input, Button, Divider, Select} from 'antd';
+import { useClassWrapper, useFetchWrapper } from '_helpers';
+
+const {Option} = Select;
 
 export {DownloadForm} 
 
 function DownloadForm(props) {
+    const [semesterData, setSemesterData] = useState([]);
+    const [semFilterState, setSemFilterState] = useState(null);
+    const fetchWrapper = useFetchWrapper();
+    
 
     const [form] = Form.useForm();
+
+    useEffect(() =>{
+      console.log("Reconstruct GPADash")
+
+      async function getSemesterData(){
+          var tempSem = [];
+          let response = await fetchWrapper.get("http://localhost:3000/api/semesters/all", null, null);
+          response = await response.json();
+          console.log(response);
+          if (response?.status === "Success"){
+              for (const object of response.message ) {
+                  tempSem.push({
+                      label: object.semester_name,
+                      value: object.semester_id
+                  })
+              }
+              setSemesterData(tempSem);
+              if (tempSem.length>0){
+                setSemFilterState(tempSem[0]);
+              }
+              
+          }
+      }
+
+      async function initDownloadForm(){
+        await getSemesterData();
+      }
+      initDownloadForm();
+    }, [props]);
+
+    function handleSemFilterChange(value) {
+      setSemFilterState(value);
+      console.log(semFilterState);
+    }
 
     function handleSubmit() {
         form.validateFields()
@@ -35,27 +75,17 @@ function DownloadForm(props) {
                     modifier: 'public',
                 }}
             >
-        <i>Nhập mã kì học gồm 5 chữ số.</i><br/>
-        <i>Ví dụ: 20201,20202...</i><br/><br/>
           <Form.Item
             name="semesterId"
             label="Mã kì học:"
-            rules={[
-              {
-                required: true,
-                message: 'Ghi mã kì học ở đây',
-              },
-              {
-                number: true,
-                message: 'Chỉ được nhập chữ số',
-              },
-              {
-                maxLength: 5,
-                message: 'Số kí tự không hợp lệ',
-              }
-            ]}
-            >
-            <Input />
+          >
+            <Select defaultValue={semFilterState} style={{ width: 180 }} onChange={handleSemFilterChange} >
+                            {semesterData.map(({ label, value }) => (
+                                <option key={value} value={value}>
+                                    { label}
+                                </option>
+              ))}
+            </Select>
           </Form.Item>
           {/* <p>Thêm nhiều sinh viên cách nhau bằng dấu ',' </p>
           <p>Sinh viên được thêm vào lớp phải có tài khoản VNU được cấp bởi admin của hệ thống. </p> */}
